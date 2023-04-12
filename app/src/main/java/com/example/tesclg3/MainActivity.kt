3451d6c96d8a9fcc9d2f6efeff304c458c57e123
adee <muhammad.adeiqbal911@gmail.com>
2023-04-12 20:13:30 +0700

    ..

--- app/src/main/java/com/example/tesclg3/MainActivity.kt
+++ app/src/main/java/com/example/tesclg3/MainActivity.kt
@@ -3,11 +3,20 @@ package com.example.tesclg3
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tesclg3.databinding.ActivityMainBinding
+import com.example.tesclg3.databinding.FragmentListAbjadBinding
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("RestrictedApi")
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val FragmentListAbjadBinding = FragmentListAbjad()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragmentContainerView, FragmentListAbjadBinding)
+            .commit()
     }
 }
